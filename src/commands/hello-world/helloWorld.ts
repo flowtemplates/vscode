97e4d565271d89668182fbc5e04c254d97f5bd3f
Nick <rudbsm@gmail.com>
2025-03-07 18:27:05 +0300

    Add assets

--- src/commands/hello-world/helloWorld.ts
+++
@@ -1,6 +0,0 @@
-import * as vscode from "vscode";
-
-export const helloWorld = (data: any[]) => {
-  console.log(data);
-  vscode.window.showInformationMessage("Hello World from Templates Flow!");
-};
